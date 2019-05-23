de3a1883b4ae7bb28d92761712426649f2c72413
Rob Phillips <robbie.phillips@lmco.com>
2019-05-23 01:35:08 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 import os
-import csv
 import pandas as pd
 import numpy as np
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
-
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Table, Column, String, create_engine, MetaData
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
+import logging
 
 app = Flask(__name__)
 
@@ -17,27 +16,28 @@ app = Flask(__name__)
 #################################################
 # Database Setup
 #################################################
-# Need to read color labels and breed labels
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pet_adoptions.sqlite"
-db = SQLAlchemy(app)
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(db.engine, reflect=True)
-
-# Save references to each table
-#Colors = Base.classes.color_labels
-#Breeds = Base.classes.breed_labels
-Adoptions = Base.classes.train
-
-
-
 
 breed_file= "static/data/breed_labels.csv"
 breed_df= pd.read_csv(breed_file)
 
+color_file= "static/data/color_labels.csv"
+color_df= pd.read_csv(color_file)
+
+train_file= "static/data/train.csv"
+train_df= pd.read_csv(train_file)
+
+postcode_file= "static/data/geocoded_state_labels.csv"
+postcode_df= pd.read_csv(postcode_file)
+
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+engine = create_engine("sqlite:///pet_adoptions.sqlite", echo=False)
+
+#breed_df.to_sql(name='breed_labels', con=engine, if_exists='append', index=True)
+#color_df.to_sql(name='color_labels', con=engine, if_exists='append', index=True)
+#train_df.to_sql(name='train', con=engine, if_exists='append', index=True)
+#postcode_df.to_sql(name='state_labels', con=engine, if_exists='append', index=True)
 
 
 @app.route("/")
@@ -45,52 +45,86 @@ def index():
     """Return the homepage."""
     return render_template("index.html")
 
+@app.route("/metadata/<attribute>,<sample>")
+def sample_metadata(attribute, sample):
+    """Return the MetaData for a given sample."""
+    if (attribute == "Age"): return jsonify(attribute, sample, 255)
+    if (attribute == "Breed1"): return jsonify(attribute, sample, str(len(breed_df)))
+    if (attribute == "Breed2"): return jsonify(attribute, sample, str(len(breed_df)))
+    if (attribute == "Gender"): return jsonify(attribute, sample, 2)
+    if (attribute == "Color1"): return jsonify(attribute, sample, str(len(color_df)))
+    if (attribute == "Color2"): return jsonify(attribute, sample, str(len(color_df)))
+    if (attribute == "Color3"): return jsonify(attribute, sample, str(len(color_df)))
+    if (attribute == "MaturitySize"): return jsonify(attribute, sample, 4)
+    if (attribute == "FurLength"): return jsonify(attribute, sample, 3)
+    if (attribute == "Vaccinated"): return jsonify(attribute, sample, 3)
+    if (attribute == "Dewormed"): return jsonify(attribute, sample, 3)
+    if (attribute == "Sterilized"): return jsonify(attribute, sample, 3)
+    if (attribute == "Health"): return jsonify(attribute, sample, 3)
+    if (attribute == "Quantity"): return jsonify(attribute, sample, 20)    
+    if (attribute == "Fee"): return jsonify(attribute, sample, 3000)
+    if (attribute == "State"): return jsonify(attribute, sample, 41415)
+    if (attribute == "RescuerID"): return jsonify(attribute, sample, str(len(train_df)))    
+    if (attribute == "VideoAmt"): return jsonify(attribute, sample, 8)
+    if (attribute == "Description"): return jsonify(attribute, sample, str(len(train_df)))
+    if (attribute == "PetID"): return jsonify(attribute, sample, str(len(train_df)))
+    if (attribute == "PhotoAmt"): return jsonify(attribute, sample, 30)
+    if (attribute == "AdoptionSpeed"): return jsonify(attribute, sample, 4)
+
+
 
 @app.route("/Adoptions")
 def Adoptions():
-    """Return a list of sample adoptions."""
+    """Return a list of adoption metadata."""
 
-    # Use Pandas to perform the sql query
-    stmt = db.session.query(Adoptions).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+    # Return a list of the column adoptions 
+    return jsonify(list(train_df.columns)[2:])
 
-    # Return a list of the column adoptions (sample adoptions)
-    return jsonify(list(df.columns)[2:])
+@app.route("/api/v1.0/Colors")
+def Pet_Colors():
 
-
-@app.route("/colors/<sample>")
-def Pet_Colors(sample):
+    try:
+       result = engine.execute("SELECT ColorID, ColorName from color_labels")
+    except:
+        print_error(f":: Could not connect!")
+    return jsonify("Pet Colors:", str(len(color_df.ColorID)), list(color_df.ColorName))
+    
+@app.route("/api/v1.0/color/<sample>")
+def Pet_Color(sample):
     """Return the MetaData for a given sample."""
-    sel = [
-        Pet_Colors.ColorID,
-        Pet_Colors.ColorName,
-    ]
 
-    results = db.session.query(*sel).filter(Pet_Colors.ColorName == Adoptions).all()
+    pet_color = str(int(sample) - 1) + ' < ColorID < ' + str(int(sample) + 1)
 
-    # Create a dictionary entry for each row of pet adoption
-    adopted_colors = {}
-    for result in results:
-        adopted_colors["black"] = result[0]
-        adopted_colors["brown"] = result[1]
-        adopted_colors["golden"] = result[2]
-        adopted_colors["yellow"] = result[3]
-        adopted_colors["cream"] = result[4]
-        adopted_colors["gray"] = result[5]
-        adopted_colors["white"] = result[6]
+    try:
+        result = engine.execute("SELECT ColorName from color_labels where ColorID = " + str(sample))
+    except:
+        print_error(f":: Could not connect!")
+    return jsonify("Pet Color:", list(color_df.query(pet_color).ColorID), list(color_df.query(pet_color).ColorName))
 
-    print(adopted_breeds)
-    return jsonify(sample_adopted_breeds)
+@app.route("/api/v1.0/Breeds")
+def Pet_Breeds():
+    dog_breed = 'Type < ' + str(2)
+    cat_breed = 'Type > ' + str(1)
 
-
-@app.route("/breeds/<sample>")
-def Pet_Breeds(sample):
-    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-    stmt = db.session.query(Adoptions).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+    try:
+       result = engine.execute("SELECT BreedID, BreedName from breed_labels")
+    except:
+        print_error(f":: Could not connect!")
+    return jsonify("Dog Breeds:", str(len(breed_df.BreedID)), list(breed_df.query(dog_breed).BreedID), list(breed_df.query(dog_breed).BreedName),
+    "Cat Breeds:", list(breed_df.query(cat_breed).BreedID), list(breed_df.query(cat_breed).BreedName))
 
 
-    return jsonify(data)
+@app.route("/api/v1.0/breed/<sample>")
+def Pet_Breed(sample):
+   
+    pet_breed = str(int(sample) - 1) + ' < BreedID < ' + str(int(sample) + 1)
+
+    try:
+       result = engine.execute("SELECT BreedName from breed_labels where BreedID = " + str(sample))
+    except:
+        print_error(f":: Could not connect!")
+    return jsonify("Pet Breed:", list(breed_df.query(pet_breed).BreedID), list(breed_df.query(pet_breed).BreedName))
+
 
 
 if __name__ == "__main__":
